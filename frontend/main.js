cdf6e39683db2019cc87d18202d4d29aaabab778
Chiu Yong <chiu@vert-capital.com>
2023-09-02 18:03:33 -0300

    feat: front side form validation

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,10 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import Login from './modules/Login'
 
-import './assets/css/style.css';
+const login = new Login('.form-login')
+const register = new Login('.form-register')
+login.init()
+register.init()
+
+// import './assets/css/style.css';
