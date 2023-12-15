135df3a941fcf6cf255ec41f6a8d8a82471f7b19
Chiu Yong <chiu@vert-capital.com>
2023-12-15 14:36:56 -0300

    fix console log

--- server.js
+++ server.js
@@ -1,60 +1,61 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes')
-const path = require('path')
+const routes = require("./routes");
+const path = require("path");
 // const helmet = require('helmet')
-const csrf = require('csurf')
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware')
-const mongoose = require('mongoose');
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const flash = require('connect-flash')
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("./src/middlewares/middleware");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 
-
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(
-    () => {
-      app.emit('pronto');
-    }
-  )
-  .catch(e => console.log(e))
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
 
 const sessionOptions = session({
-  secret: 'agenda-project',
+  secret: "agenda-project",
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true
-  }
-})
+    httpOnly: true,
+  },
+});
 
 // app.use(helmet())
-app.use(sessionOptions)
-app.use(flash())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'public')))
-app.set('views', path.resolve(__dirname, 'src', 'views'))
-app.set('view engine', 'ejs')
-app.use(csrf())
+app.use(sessionOptions);
+app.use(flash());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "public")));
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+app.use(csrf());
 
-app.use(middlewareGlobal)
-app.use(checkCsrfError)
-app.use(csrfMiddleware)
-app.use(routes)
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(routes);
 
-app.get('/testes/:idUsuarios?/:parametros?', (req, res) => {
-  res.send(req.query.nome)
-})
+app.get("/testes/:idUsuarios?/:parametros?", (req, res) => {
+  res.send(req.query.nome);
+});
 
-app.on('pronto', () => {
-  app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
-  })
-})
-
+app.on("pronto", () => {
+  app.listen(3000, () => {});
+});
